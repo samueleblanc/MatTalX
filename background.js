0f6845cb2860119ba15f014cae3ee8c905cc6c34
samueleblanc <86082064+samueleblanc@users.noreply.github.com>
2022-05-09 18:09:40 -0400

    removed commands to copy text

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 /*
-    Listens for new update, first time users and keyboard shortcuts
-    Sends a message to "convert.js" if any of these happens
+    Listens for new update and first time users
+    Sends a message to "popup.js" if any of these happens
 */
 
 // Tells the user if it's a new version of MatTalX, or some info if they are first time users
@@ -9,19 +9,6 @@ chrome.runtime.onInstalled.addListener((details) => {
         chrome.runtime.sendMessage("installed");
     } else if (details.reason == "update") {
         chrome.runtime.sendMessage("updated");
-    };
-}
-);
-
-// Lets you copy the text in (first box) or text out (second box) with simple commands
-// (Alt+1 and Alt+2 respectively)
-chrome.commands.onCommand.addListener((command) => {
-    if (command == "copy-text-in") {
-        chrome.runtime.sendMessage("copyFirst");
-    } else if (command == "copy-text-out") {
-        chrome.runtime.sendMessage("copySecond");
-    } else if (command == "close-popup") {
-        chrome.runtime.sendMessage("closePopup");
     };
 }
 );
