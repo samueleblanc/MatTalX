42e3d6542ca06f5edfd90d9f49d3bede92432fbf
Samuel Leblanc <samueleblanc17@gmail.com>
2022-10-19 07:47:54 -0400

    build for website

--- build.sh
+++ build.sh
@@ -2,32 +2,56 @@
 
 # Creates a directory and a zip file for either a chrome extension or firefox addon
 
+chrome_ext () {
+    mkdir extension
+    cp -RT common extension
+    cp -RT chrome extension
+    cat extension/messages.js >> extension/popup.js
+    cat extension/popup-specific.js >> extension/popup.js
+    rm extension/messages.js
+    rm extension/popup-specific.js
+    zip -r -q extension.zip extension
+}
+
+firefox_addon () {
+    mkdir addon
+    cp -RT common addon
+    cp -RT firefox addon
+    cat addon/messages.js >> addon/popup.js
+    cat addon/popup-specific.js >> addon/popup.js
+    rm addon/messages.js
+    rm addon/popup-specific.js
+    zip -r -q addon.zip addon
+}
+
+website () {
+    cat common/popup.js > ~/Website/mattalx/convert-text.js
+    cat web_version/web-specific.js >> ~/Website/mattalx/convert-text.js
+    cat web_version/web-version.html > ~/Website/mattalx/web-version.html
+    cat web_version/web-version.css > ~/Website/mattalx/web-version.css
+    cd ~/Website/mattalx
+    git commit -a -m "update to newest version"
+    git push origin master
+}
+
 if  [[ $1 == "firefox" ]]; then
     if [[ -e addon ]]; then
         echo "'addon' directory already exists"
     else
-        mkdir addon
-        cp -RT common addon
-        cp -RT firefox addon
-        cat addon/messages.js >> addon/popup.js
-        cat addon/popup-specific.js >> addon/popup.js
-        rm addon/messages.js
-        rm addon/popup-specific.js
-        zip -r -q addon.zip addon
+        firefox_addon
     fi
 elif [[ $1 == "chrome" ]]; then
     if [[ -e extension ]]; then
         echo "'extension' directory already exists"
     else
-        mkdir extension
-        cp -RT common extension
-        cp -RT chrome extension
-        cat extension/messages.js >> extension/popup.js
-        cat extension/popup-specific.js >> extension/popup.js
-        rm extension/messages.js
-        rm extension/popup-specific.js
-        zip -r -q extension.zip extension
+        chrome_ext
     fi
+elif [[ $1 == "website" ]]; then
+    website
+elif [[ $1 == "all" ]]; then
+    firefox_addon
+    chrome_ext
+    website
 else
-    echo "The only accepted parameter is either 'chrome' or 'firefox'."
+    echo "Accepted arguments: 'firefox', 'chrome', 'website' or 'all'."
 fi
