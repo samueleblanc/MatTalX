c602b35312de32c5c1e2b7db8f3fe7d3d32c5545
Samuel Leblanc <samueleblanc17@gmail.com>
2022-12-27 13:50:35 -0500

    inner loop

--- common/parser.ts
+++ common/parser.ts
@@ -5,7 +5,7 @@
 
 import { mathDictionary } from "./unicode/mathsymbols";
 import { addSymbol, prohibitedType, mistakes } from "./convert"
-import { dict } from "./types";
+import { dict, Fct, Str } from "./types";
 
 // MAIN
 
@@ -21,20 +21,34 @@ function parseInput(fullText: string, plainTextConverter: dict): string {
     let numberCurly: number = 0;
     let trigger: boolean = false;
     let temporaryBox: string[] = [];
+    let t: Token;
     let outputBox: Token[] = [];
     const parentheses: string[] = ["(", ")"];
     const brackets: string[] = ["[", "]"];
-    const commandStoppers: string[] = [" ", ",", "/", "-", "+", "=", "<", ">", "|", "?", "!"];  // parentheses and brackets also stops commands (most of the time)
+    const commandStoppers: string[] = [" ", ",", "/", "-", "+", "=", "<", ">", "|", "?", "!", "\\"];  // parentheses and brackets also stops commands (most of the time)
     const needBackslash: string[] = ["$", "%", "#"];  // inside *and* outside of mathmode
 
     let i: number;
     for (i=0; i<fullText.length; i++) {
         if (mathmode) {
             if (fullText[i] === "\\") {
-                
+                if (fullText[i-1] === "\\") {
+                    t = {math: mathmode, depth: d, symbol: Str(mathDictionary[temporaryBox.join("") + fullText[i-1]])};
+                    outputBox.push(t);
+                }
             };
         } else {
-            if (fullText[i] === "\\") {
+            if (fullText[i] === "$") {
+                if (fullText[i-1] === "$") {  // TODO: $$$ shouldn't be allowed
+
+                } else if (fullText[i-1] === "\\") {
+
+                } else {
+
+                };
+            } else if (fullText[i] === "[") {
+
+            } else if (fullText[i] === "\\") {
 
             };
         };
