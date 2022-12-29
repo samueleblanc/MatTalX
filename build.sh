2ead7196e59607334341b1b125e602bad03d1f50
Samuel Leblanc <samueleblanc17@gmail.com>
2022-12-29 11:11:54 -0500

    typescript compiling and andoid build

--- build.sh
+++ build.sh
@@ -3,20 +3,59 @@
 # Creates a directory and a zip file for either a chrome extension or firefox add-on
 # Can also test MatTalX, however, it's not a complete test
 
+compile_common () {
+    for file in common/*.ts; do
+        tsc common/$file  # TypeScript compiler
+        echo "$file compiled"
+    done
+    for file in common/packages/*.ts; do
+        tsc common/packages/$file
+        echo "Packages compiled"
+    done
+    for file in common/unicode/*.ts; do
+        tsc common/unicode/$file
+        echo "Unicode files compiled"
+    done
+}
 
-## IMPORTANT: This file is currently useless, it needs a lot of changes due to the complete restructuring of MatTalX
+copy_common () {
+    for file in common; do
+        case $file in
+            *.js)
+            mv common/$file $1  # Parameter is the name of the directory (e.g. 'extension', 'firefox_add_on', etc.)
+            ;;
+            *.html | *.css)
+            cp common/$file $1
+            ;;
+            *)
+            continue  # i.e. don't copy .ts (or other) files
+            ;;
+        esac
+    done
+    for file in common/packages/*.js; do
+        mv common/packages/$file $1
+    done
+    for file in common/unicode/*.js; do
+        cp common/unicode/$file $1
+    done
+    for file in common/images; do
+        cp common/images/$file $1
+    done
+    echo "Files from 'common' transfered"
+}
 
 chrome_ext () {
     if [[ -e extension ]]; then
         echo "'extension' directory already exists"
     else
-        echo "Creating directory..."
+        echo "Compiling..." 
+        compile_common
+        echo "Compilation done"
+        echo "Creating 'extension' directory..."
         mkdir extension
-        cp -RT common extension
+        copy_common extension
         cp -RT chrome extension
-        cat extension/messages.js >> extension/popup.js
         cat extension/popup-specific.js >> extension/popup.js
-        rm extension/messages.js
         rm extension/popup-specific.js
         echo "Directory made"
         echo "Compressing..."
@@ -26,29 +65,50 @@ chrome_ext () {
 }
 
 firefox_addon () {
-    if [[ -e addon ]]; then
-        echo "'addon' directory already exists"
+    if [[ -e firefox_add_on ]]; then
+        echo "'firefox_add_on' directory already exists"
     else
-        echo "Creating directory..."
-        mkdir addon
-        cp -RT common addon
-        cp -RT firefox addon
-        cat addon/messages.js >> addon/popup.js
-        cat addon/popup-specific.js >> addon/popup.js
-        rm addon/messages.js
-        rm addon/popup-specific.js
+        echo "Compiling..." 
+        compile_common
+        echo "Compilation done"
+        echo "Creating 'firefox_add_on' directory..."
+        mkdir firefox_add_on
+        copy_common firefox_add_on
+        cp -RT firefox/main firefox_add_on
+        cat firefox_add_on/popup-specific.js >> firefox_add_on/popup.js
+        rm firefox_add_on/popup-specific.js
         echo "Directory made"
         echo "Compressing..."
-        cd addon; zip -r ../addon.zip .
+        cd firefox_add_on; zip -r ../firefox_add_on.zip .
+        echo "--Done--"
+    fi
+}
+
+android_addon () {
+    if [[ -e andoid_add_on ]]; then
+        echo "'android_add_on' directory already exists"
+    else
+        echo "Compiling..." 
+        compile_common
+        echo "Compilation done"
+        echo "Creating 'android_add_on' directory..."
+        mkdir android_add_on
+        copy_common andoid_add_on
+        cp -RT firefox/android andoid_add_on
+        cat andoid_add_on/popup-specific.js >> andoid_add_on/popup.js
+        rm andoid_add_on/popup-specific.js
+        echo "Directory made"
+        echo "Compressing..."
+        cd andoid_add_on; zip -r ../andoid_add_on.zip .
         echo "--Done--"
     fi
 }
 
 # Will soon be removed
 website () {
-    echo "Creating directory..."
+    echo "Creating 'webtest' directory..."
     mkdir webtest
-    cp -r common/Images webtest
+    cp -r common/images webtest
     cp common/popup.js webtest
     cat web_version/web-specific.js >> webtest/popup.js
     mv webtest/popup.js webtest/convert-text.js
@@ -58,20 +118,24 @@ website () {
 }
 
 testing () {
-    if [[ -e mattalx_test ]]; then
-        rm -r mattalx_test
+    # Might use this in the future
+    #   https://extensionworkshop.com/documentation/develop/getting-started-with-web-ext/
+    if [[ -e test_mattalx ]]; then
+        rm -r test_mattalx
     fi
-    echo "Creating directory..."
+    echo "Compiling..." 
+    compile_common
+    echo "Compilation done"
+    echo "Creating 'test_mattalx' directory..."
     mkdir test_mattalx
-    cp -r common/Images test_mattalx
-    cp common/popup.js test_mattalx
-    cp common/popup_style.css test_mattalx
-    cat web_version/web-specific.js >> test_mattalx/popup.js
-    cp chrome/popup.html test_mattalx
-    sed -i '7d' test_mattalx/popup.html
+    copy_common test_mattalx
+    cp -RT firefox/main test_mattalx
+    cat test_mattalx/popup-specific.js >> test_mattalx/popup.js
+    rm test_mattalx/popup-specific.js
     mv test_mattalx/popup.html test_mattalx/index.html
-    sed -i '37d' test_mattalx/index.html
-    sed -i '37d' test_mattalx/index.html
+    sed '$d' test_mattalx/index.html  # [sed '$d'] doesn't work on Mac
+    sed '$d' test_mattalx/index.html
+    # sed -i '' -e '$ d' test_mattalx/index.html  # For Mac users
     cat test.txt >> test_mattalx/index.html
     echo "Directory made"
     echo "Opening MatTalX on localhost"
@@ -82,16 +146,18 @@ testing () {
 
 if  [[ $1 == "firefox" ]]; then
     firefox_addon
+elif  [[ $1 == "android" ]]; then
+    android_addon
 elif [[ $1 == "chrome" ]]; then
     chrome_ext
 elif [[ $1 == "website" ]]; then
     website
 elif [[ $1 == "b-all" ]]; then
     firefox_addon
+    android_addon
     chrome_ext
-    website
 elif [[ $1 == "test" ]]; then
     testing
 else
-    echo "Accepted arguments: 'firefox', 'chrome', 'website', 'b-all' or 'test'." # b-all stands for build all
+    echo "Accepted arguments: 'firefox', 'android', 'chrome', 'website', 'b-all' or 'test'." # b-all stands for build all
 fi
