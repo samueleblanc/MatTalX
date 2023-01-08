7ce4e26884deefe41ebc69bd9b10d3ae21f8a2e6
Samuel Leblanc <samueleblanc17@gmail.com>
2023-01-08 00:18:04 -0500

    v 2.5.0

--- v2/common/messages.js
+++ v2/common/messages.js
@@ -10,8 +10,8 @@ function firstMessage(version) {
     "on the top right corner. MatTalX almost always uses the same command as LaTeX, but there are some differences.";
     const tutorial = "First and foremost, it is important to remember that MatTalX converts the command into text, therefore a command like: \r\n" + 
     "x^{x^{x^{x}}} will give an error, since it can't fit in a line.\r\n \r\n" +
-    "Every letter will automatically be converted to a mathematical font, if you do not want that, you can put the text inside the command \\text{}. " + 
-    "If you simply want a single letter to not be in the mathematical font, add '\\' before the letter:\r\n" + 
+    "Every letter will automatically be converted to a mathematical font, if you do not want that, you can uncheck 'Mathematical font'. " + 
+    "If you simply want a single letter to not be in this font, add '\\' before the letter:\r\n" + 
     " a → \u{1d44e} | \\a (or \\text{a}) → a\r\n \r\n" + 
     "As a last tip, if you get an error and the command seems right, the character that you want might not exist in unicode:\r\n" + 
     " x^{y} → 𝑥ʸ | x^{Y} → 𝑥^(𝑌) (ie y exists in superscript but not Y)\r\n \r\n" +
@@ -26,7 +26,11 @@ function updateMessage(version) {
     // To be changed by hand every version
     const majorChanges = "Welcome to MatTalX version " + version + "\r\n \r\n" + 
     "Major changes: \r\n" +
-    " 1) Better error messages\r\n" + 
-    " 2) Improved 'Adjust spaces'";
+    " 1) Colors automatically change based on the user's prefered color scheme (light or dark)\r\n" + 
+    " 2) It is now possible to convert (or not) characters in a mathematical font\r\n" + 
+    " 3) By unchecking 'Math mode', you can now work outside of the math environement. Use '$', '$$' or '\\[' to enter " + 
+    "math mode and '$', '$$' and '\\]' respectively to exit\r\n" + 
+    " 4) If the suggestion popup is open, you can now close it by clicking outside of the box\r\n" + 
+    " 5) MatTalX is now supported on android (Firefox)";
     textOut.value = majorChanges;  // In "text_input" form
 };
