5d195cb7bd845972d32e8e82647b6004195616f5
Samuel Leblanc <samueleblanc17@gmail.com>
2023-01-07 21:34:41 -0500

    new buttons for mathmode and mathfont

--- v2/firefox/popup-specific.js
+++ v2/firefox/popup-specific.js
@@ -1,46 +1,48 @@
 /*
-    Functions specific to Chrome
-
+    Functions specific to Firefox
+    
     This file is copy-pasted in popup.js
     Therefore, a special attention to variable names is needed.
 
-    These functions differs from their Firefox counterpart as the API for storing data is slightly different
+    These functions differs from their Chrome counterpart as the API for storing data is slightly different
     and because shortcuts are different
 */
 
-document.getElementById("short_open_mattalx").textContent = "Ctrl+M : Open/Close MatTalX";
+document.getElementById("short_open_mattalx").textContent = "Alt+M : Open/Close MatTalX";
 document.getElementById("short_copy_input").textContent = "Alt+I : Copy input (first box)";
 document.getElementById("short_copy_output").textContent = "Alt+O : Copy ouput (second box)";
-document.getElementById("short_open_suggestions").textContent = "Alt+S : Open/Close suggestions";
+document.getElementById("short_open_suggestions").textContent = "Alt+C : Open/Close completion";
 
 window.addEventListener("blur", () => {
     // Saves the text in the first box so it doesn't disappear if you change page or close MatTalX
-    chrome.storage.sync.set({"box1" : textIn.value});
-    chrome.storage.sync.set({"spaces" : spacesButton.checked});
-    chrome.storage.sync.set({"font" : changeFontButton.checked});
-    chrome.storage.sync.set({"mode" : changeModeButton.checked});
+    browser.storage.local.set({
+        "box1" : textIn.value,
+        "spaces" : spacesButton.checked,
+        "font" : changeFontButton.checked,
+        "mode" : changeModeButton.checked
+    });
 });
 
 window.addEventListener("focus", () => {
     // Retreives the text when the popup reopens
-    chrome.storage.sync.get(["box1"], (text) => {
+    browser.storage.local.get("box1", (text) => {
         if (text.box1 !== undefined) {
             textIn.value = text.box1;
         };
     });
-    chrome.storage.sync.get(["spaces"], (button) => {
+    browser.storage.local.get("spaces", (button) => {
         // Default is true
         if (button.spaces === false) {
             spacesButton.checked = false;
         };
     });
-    chrome.storage.sync.get(["font"], (button) => {
+    browser.storage.local.get("font", (button) => {
         // Default is true
         if (button.font === false) {
             changeFontButton.checked = false;
         };
     });
-    chrome.storage.sync.get(["mode"], (button) => {
+    browser.storage.local.get("mode", (button) => {
         // Default is true
         if (button.mode === false) {
             changeModeButton.checked = false;
@@ -51,15 +53,15 @@ window.addEventListener("focus", () => {
 
 window.addEventListener("DOMContentLoaded", () => {
     // Listens for 'message' from background.js
-    const manifest = chrome.runtime.getManifest();
-    chrome.storage.local.get("reason", (details) => {
+    const manifest = browser.runtime.getManifest();
+    browser.storage.local.get("reason", (details) => {
         if (details.reason === "install") {
             firstMessage(manifest.version);
         } else if (details.reason === "update") {
             updateMessage(manifest.version);
         };
     });
-    chrome.storage.local.remove("reason");
+    browser.storage.local.remove("reason");
 });
 
 window.addEventListener("click", (event) => {
@@ -72,9 +74,9 @@ window.addEventListener("click", (event) => {
 });
 
 document.addEventListener("keydown", (keyPressed) => {
-    // Listens for Alt+S to show suggestions, Alt+I to copy text of the first box (input) and Alt+O to copy text in the second box (output)
-    if ((keyPressed.key === "s") && keyPressed.altKey && (textIn == document.activeElement)) {
-        // Alt+S to shows suggestions but closes the popup if the suggestion box is already opened
+    // Listens for Alt+C to show suggestions, Alt+I to copy text of the first box (input) and Alt+O to copy text in the second box (output)
+    if ((keyPressed.key === "c") && keyPressed.altKey && (textIn == document.activeElement)) {
+        // Alt+C to shows suggestions but closes the popup if the suggestion box is already opened
         getSuggestion();
     } else if ((keyPressed.key === "i") && keyPressed.altKey) {
         copyTextIn();
